e004e9be4a7b5d87abd91c2a935e3e720242150b
ecurtis28 <evancurtis28@protonmail.com>
2023-07-09 20:54:47 -0700

    testing if I can utilize a webservice alone instead of a monorepository 2

--- multiplayer_chess_backend/src/index.js
+++ multiplayer_chess_backend/src/index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: `http://localhost:3000`,
+    origin: `http://multiplayer-chess-site.onrender.com`,
     // location of frontend (need to somehow specify port to render so that this code works)
     methods: ["GET", "POST"],
   },
